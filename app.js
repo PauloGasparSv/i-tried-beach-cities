0c442be0b4e9122c39ba343cfd814ec3b937959b
Paulo Vale <paulogaspar404@gmail.com>
2019-08-30 23:31:07 -0300

    Added npm start script. Now using path to join the directories so that this code runs on windows. Now showing how long the process takes. Added a NO_CACHE env variable that controls wheter or not to rewrite the .cache folder.

--- app.js
+++ app.js
@@ -1,49 +1,63 @@
 'use strict';
 const http = require('./http');
+const path = require('path');
 const { JSDOM } = require('jsdom');
 const fs = require('fs');
 const wikiBeaches = 'https://en.wikipedia.org/wiki/List_of_beaches';
 const wikiAirports = 'https://pt.wikipedia.org/wiki/Lista_de_aeroportos_por_pa%C3%ADs';
 
 function checkCache() {
-    if (!fs.existsSync('./.cache')) {
-        fs.mkdirSync('./.cache');
+    if (process.env.NO_CACHE === '1') {
+        const files = fs.readdirSync('.cache');
+        for (let f of files ) {
+            fs.unlinkSync(path.join('.cache', f));
+        }
+        fs.rmdirSync('.cache');
+    }
+    if (!fs.existsSync('.cache')) {
+        fs.mkdirSync('.cache');
     }
 }
 
 function getCached(file) {
-    if (!fs.existsSync(`./.cache/${file}`)) {
+    if (!fs.existsSync(path.join('.cache',file))) {
         return null;
     }
-    return fs.readFileSync(file);
+    return fs.readFileSync(path.join('.cache',file));
 }
 
 function cache(file, content) {
-    return fs.writeFileSync(`./.cache/${file}`, content);
+    return fs.writeFileSync(path.join('.cache',file), content);
 }
 
 (
 async function main() {
     try {
         checkCache();
-        console.log('Loading beaches page');
+        const startTime = new Date().getTime();
+        console.log('>>>> Started Loading Beaches Page');
         let beachPage = getCached('beachPage');
         if (!beachPage) {
             beachPage = await http.get(wikiBeaches);
             beachPage = beachPage.body;
             cache('beachPage', beachPage);
         }
-        console.log('Loading airports page');
+        console.log('>>>> Finished Loading Beaches Page');
+        console.log('>>>> Started Loading Airports Page');
         let airportPages = getCached('airportPages');
         if (!airportPages) {
             airportPages = await http.get(wikiAirports);
             airportPages = airportPages.body;
             cache('airportPages', airportPages);
         }
-        console.log('Traversing through beaches');
+        console.log('>>>> Finished Loading Airports Page');
         let { document } = (new JSDOM(beachPage)).window;
         console.log(document.querySelector('#firstHeading').textContent);
+        const endTime = new Date().getTime();
+
+        console.log(`Whole process took ${(endTime - startTime)/1000} seconds.`)
     } catch(e) {
+        console.log(e);
         return;
     }
 }
