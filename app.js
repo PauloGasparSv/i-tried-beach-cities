5a679c155f5317b2897b0139294d347d4da3066f
Paulo Vale <paulogaspar404@gmail.com>
2019-09-01 19:05:16 -0300

    Can't find out if the address is coastal.

--- app.js
+++ app.js
@@ -4,67 +4,92 @@ const cache = require('./cache');
 const wikiAirports = 'https://en.wikipedia.org/wiki/List_of_airports';
 const startTime = new Date().getTime();
 
-// Main function.
-// Anonymous because I wanted to use await.
 (
     async function main() {
         try {
             // Checks if the .cache folder exists and wether or not to delete it.
             cache.check();
-            await loadAirports();
-            time();
+            // Getting every airport page on wikipedia.
+            let airports = await loadAirports();
+
+            // Since this will take forever, i'll do the process with only one airport.
+            airports = [ airports[0] ];
+
+            for(let airport of airports) {
+                await search(airport);
+            }
+
+            // Registering how long the code took to run.
+            const endTime = new Date().getTime();
+            console.log(`Whole process took ${(endTime - startTime)/1000} seconds.`);
         } catch(e) {
-            console.log('Error, finish.');
+            console.error(e);
+            console.error('Could not finish crawling.');
         }
     }
 )();
 
-// Registers how much time this script took to run.
-function time() {
-    const endTime = new Date().getTime();
-    console.log(`Whole process took ${(endTime - startTime)/1000} seconds.`)
-}
-
 // Retrieves the list of airports of the world
 function loadAirports() {
     return new Promise(async (resolve, reject) => {
-        browser.loadPage(wikiAirports)
-        .then(page => {
+        try {
+            // Downloading the airport list indexing page from wikipedia
+            const page = await browser.loadPage(wikiAirports);
             const document = browser.getDocument(page);
+            // Getting the link to the first page of airports.
             let link = document
                 .querySelector('.mw-selflink.selflink')
                 .nextElementSibling
                 .nextElementSibling;
+            // Preparing an array to store the page for each list.
             const links = [];
             while (link) {
-                links.push(link.getAttribute('href'));
+                links.push(wikiAirports.split('/wiki')[0] + link.getAttribute('href'));
                 link = link.nextElementSibling;
             }
+            // Preparing an array to store each individual airport wiki page link
             const airports = [];
-
-            link = links.splice(1);
-
-            links.forEach(async link => {
-                try {
-                    const pg = await browser.loadPage(link);
-                    const doc = browser.getDocument(pg);
-                    doc.querySelectorAll('table tr').forEach(tr => {
-                        if (!tr.querySelector('td')) {
-                            return;
-                        }
-                        const ICAO = tr.querySelector('td:nth-child(2)');
-                        const airport = tr.querySelector('td:nth-child(3)');
-                        airports.push({
-                            ICAO,
-                            airport
-                        })
-                    });
-                } catch(e) {
-                    console.error('Error during ' + link);
+            for (let link of links) {
+                const pg = await browser.loadPage(link);
+                const doc = browser.getDocument(pg);
+                for (let tr of doc.querySelectorAll('table tr')) {
+                    const airport = tr.querySelector('td:nth-child(3) a');
+                    if(!airport) {
+                        continue;
+                    }
+                    const href = airport.getAttribute('href');
+                    if(!href || !href.includes('wiki')) {
+                        continue;
+                    }
+                    airports.push(wikiAirports.split('/wiki')[0] + airport.getAttribute('href'));
                 }
-            });
+            }
+            // Returning all the links
             resolve(airports);
-        })
-        .catch(error => reject(error));
+        } catch(e) {
+            reject(e);
+        }
     });
+}
+
+async function search(airport) {
+    try {
+        // Downloading the airport list indexing page from wikipedia
+        const page = await browser.loadPage(airport);
+        const document = browser.getDocument(page);
+
+        let location = null;
+        document.querySelectorAll('.infobox th').forEach(th => {
+            if(location) {
+                return;
+            }
+            const html = th.innerHTML.toUpperCase();
+            if(html.includes('LOCATION') || html.includes('COORDINATES')) {
+                location = th.nextElementSibling.textContent;
+            }
+        });
+        console.log('My location: ' + location);
+    } catch(e) {
+        console.log(e);
+    }
 }
